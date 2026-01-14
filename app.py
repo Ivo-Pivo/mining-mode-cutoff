2c7eef4e8b5ebe056d57f4ed334a5cc1cf587b5a
Ivo-Pivo <122823327+Ivo-Pivo@users.noreply.github.com>
2026-01-14 13:41:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -96,14 +96,14 @@ st.header("Results")
 st.subheader("Mode efficiencies")
 st.table({
     "TH/kWh": {
-        "Eco": round(eco_eff, 2),
-        "Standard": round(std_eff, 2),
-        "Super": round(sup_eff, 2),
+        "Eco": round(eco_eff, -2),
+        "Standard": round(std_eff, -2),
+        "Super": round(sup_eff, -2),
     },
     "J/TH": {
         "Eco": round(eco_jth, 1),
-        "Standard": round(std_jth, 2),
-        "Super": round(sup_jth, 2),
+        "Standard": round(std_jth, 1),
+        "Super": round(sup_jth, 1),
     }
 })
 
